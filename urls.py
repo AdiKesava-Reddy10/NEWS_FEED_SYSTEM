d9fca8be8d719fad1603829927f6be29808c5299
AdiKesava Reddy <vtu25083@veltech.edu.in>
2026-04-15 11:16:20 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,39 +1,23 @@
-"""
-URL configuration for authentic_news_project project.
+from django.urls import path
+from . import views
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-# authentic_news_project/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-from django.contrib import admin
-from django.urls import path, include
-from news_feed import views as news_feed_views
+from django.urls import path
 from django.contrib.auth import views as auth_views
-
+from . import views
+from news_feed import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # Set the new landing_page as the root URL
-    path('', news_feed_views.landing_page, name='landing_page'),
-    # Include all the URLs from your news_feed app
-    path('news/', include('news_feed.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('api/add-article/', news_feed_views.add_article_api, name='add_article_api'),
+    path('', views.landing_page, name='landing_page'),
+    path("category/<slug:category>/", views.categorized_news, name="categorized_news"),
+    path('home/', views.homepage, name='homepage'),
+    path('search/', views.search_results, name='search_results'),
+    path('subscribe/', views.subscribe, name='subscribe'),
+    path('weather/', views.weather_report, name='weather_report'),
+    path('report_misinformation/', views.report_misinformation, name='report_misinformation'),
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='landing_page'), name='logout'),
+    path('api/add-article/', views.add_article_api, name='add_article_api'),
     
-]
-
-
+]   
+handler404 = 'news_feed.views.handler404'                               
